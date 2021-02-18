42b61acab1245b823d6bc56e4458a08f5cb4567e
Samir Jaber <samirjaber50@gmail.com>
2021-02-18 05:18:21 -0500

    Initial files that were supposed to be there

---
+++ TopFlix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  TopFlix
+//
+//  Created by Samir Jaber on 2/17/21.
+//  Copyright © 2021 samirjaber50@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TopFlixTests/TopFlixTests.swift
@@ -0,0 +1,34 @@
+//
+//  TopFlixTests.swift
+//  TopFlixTests
+//
+//  Created by Samir Jaber on 2/17/21.
+//  Copyright © 2021 samirjaber50@icloud.com. All rights reserved.
+//
+
+import XCTest
+@testable import TopFlix
+
+class TopFlixTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
