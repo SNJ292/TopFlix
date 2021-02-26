0136375d006d9321df3f62ab70a1c0b373e5c81f
Samir Jaber <samirjaber50@gmail.com>
2021-02-26 03:21:59 -0500

    Unit 2 of TopFlix added

---
+++ TopFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  TopFlix
+//
+//  Created by Samir Jaber on 2/25/21.
+//  Copyright © 2021 samirjaber50@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
